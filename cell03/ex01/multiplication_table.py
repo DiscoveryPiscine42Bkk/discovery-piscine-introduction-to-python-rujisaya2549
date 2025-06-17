5dd22db9094f6593c127a7ab5863b128db131461
rujisaya2549 <68030249@kmitl.ac.th>
2025-06-17 13:10:08 +0000

    3.1

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,5 +1,7 @@
-num = Int(input("Enter a number\n"))
-i-0
-while i <= 9:
-    print(f"{i} x {num} = {num}")
-    i += 1
+value = int(input())
+
+if value >= 25:
+	print("Error")
+else:
+	for num in range(value, 25):
+		print(f"Inside the loop, my variable is {num}")
