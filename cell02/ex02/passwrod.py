d7ffa04a8138396d01acd63deb070e4f91eed327
rujisaya2549 <68030249@kmitl.ac.th>
2025-06-17 12:51:14 +0000

    งานส่งคร้าบ

---
+++ cell02/ex02/passwrod.py
@@ -0,0 +1,13 @@
+value = int(input())
+
+# ทำการเช็คว่าค่าตัวเลขที่รับเข้ามามีค่ามากกว่า 0 หรือไม่
+if value > 0:
+	print("This number is positive.")
+	
+# หากเงื่อนไขแรกไม่เป็นจริง: ทำการเช็คต่อว่าค่าตัวเลขที่รับเข้ามามีค่าน้อยกว่า 0 หรือไม่
+elif value < 0:
+	print("This number is negative.")
+
+# หากทั้งสองเงื่อนไขไม่เป็นจริง: ให้ทำงานตรงนี้แทน
+else:
+	print("This number is both positive and negative.")
